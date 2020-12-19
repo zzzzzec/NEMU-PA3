d19446ef7cdb9e6685e4f75de8a8c3dc6fde1ac8
tracer-ics2016 <tracer@njuics.org>
2020-12-19 19:49:40 +0800

    >  compile NEMU 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:49:40 up  6:00,  1 user,  load average: 0.78, 0.55, 0.44 8b5250067d006d6ce6d736b067042b33129a74e1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -161,7 +161,7 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data)
 	}
 	if(find){ 
 		printf("LEN is %ld  Addr is 0x%x\n", len, addr);
-		printf("data is 0x%04x \n",data);
+		printf("data is 0x%08x \n",data);
 	    memcpy(L1[set][i].data +offset , &data ,len);
 		assert(find != true);
 	}
