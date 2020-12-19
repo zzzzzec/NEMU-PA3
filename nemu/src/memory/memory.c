b70896583a20fb0b58d9178d9376d54921fb30a5
tracer-ics2016 <tracer@njuics.org>
2020-12-19 16:59:42 +0800

    >  compile NEMU 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:59:42 up  3:10,  1 user,  load average: 0.56, 0.49, 0.44 4d6f085b2de716d28260c34a60762fb4810e0c19

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -43,7 +43,7 @@ void view_cache(uint32_t set , uint32_t line ){
 	int i;
 	for ( i = 0; i <64; i++)
 	{
-		printf("%x ",L1[set][line].data[i]);
+		printf("%02x ",L1[set][line].data[i]);
 		if(  (i+1)%16 == 0)
 		{
 			printf("\n");
