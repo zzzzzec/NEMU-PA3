7f8e4c6a8887ccdeca27c9af6399579275e0a08e
tracer-ics2016 <tracer@njuics.org>
2020-12-19 11:47:22 +0800

    >  compile NEMU 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:47:22 up  1:07,  1 user,  load average: 0.80, 0.88, 0.81 c39784242e571e0bc1942b18a7b84d7fecb49e4c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,93 +1,135 @@
 #include "common.h"
 //define some attributes
 #define BLOCK_SIZE 64 //8byte
-#define L1_SIZE  (64*1024)
-#define SET (128)                          //8-way set associate
-#define LINE 8 
+#define L1_SIZE (64 * 1024)
+#define SET (128) //8-way set associate
+#define LINE 8
+int number = 0;
 
 /*cache line :   ******21*******|| ****7****||****4****
                                                tag                    index          offset   */
-typedef struct 
+typedef struct
 {
 	bool valid;
-	 uint32_t tag;
-     uint8_t  data[BLOCK_SIZE];
-}Cache;
-Cache L1[SET][LINE];  /*cache1 1024            */
+	uint32_t tag;
+	uint8_t data[BLOCK_SIZE];
+} Cache;
+Cache L1[SET][LINE]; /*cache1 1024            */
 
-void init_cache(){
-	int i,j;
-	for ( i = 0; i < SET; i++)
+void init_cache()
+{
+	int i, j;
+	for (i = 0; i < SET; i++)
 	{
-		for ( j = 0; j < LINE; j++)
+		for (j = 0; j < LINE; j++)
 		{
-			 L1[i][j].valid = false;
-			 L1[i][j].tag = 0;
-		     memset (L1[i][j].data ,0 ,BLOCK_SIZE);
-		}   
+			L1[i][j].valid = false;
+			L1[i][j].tag = 0;
+			memset(L1[i][j].data, 0, BLOCK_SIZE);
+		}
 	}
 }
-
-
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
-/* Memory accessing interfaces */
-
-uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	/* 0u : 0000 0000 0000 0000
-	 ~0u : 1111 1111 1111 1111   */
-	bool find=false;
-    uint32_t set, ttag,offset;
-	set = (addr >> 4)&(0x7f);
-	ttag = (addr >> 11);
-    offset = (addr & 0xf);
-	int i ;
-	for ( i = 0; i < LINE; i++)
-	{
-		if(L1[set][i].valid == true && L1[set][i].tag == ttag){
-                find = true;
-				break;
-		}
-	}
-	if(find == true){
-		uint32_t result[2];
-		memcpy(result , L1[set][i].data +(4* offset),4);
-		return result[0]& (~0u >> ((4 - len) << 3));
-	}
-    else
-	{
-	/* int j =0;
-	 for(j =0;j<LINE ;j++){
-
-	 }	
-	}*/
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	  }	
+int get_num()
+{
+    number ++;
+	number = number % 8;
+	return number;
 }
 
-void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
+/* Memory accessing interfaces */
+/*copy 64B from ram to cache*/
+void M2C(hwaddr_t addr,uint32_t set ,int line){
+            uint32_t tem[16];
+			int k=0;
+			for ( k = 0; k < 16; k++)
+			{
+				tem[k] = dram_read(addr, 4);
+			}
+			memcpy(tem , L1[set][line].data , 64);
+}
+
+uint32_t hwaddr_read(hwaddr_t addr, size_t len)
+{
+	/* 0u : 0000 0000 0000 0000
+	 ~0u : 1111 1111 1111 1111   */
+	bool find = false;
+	uint32_t set, ttag, offset;
+	set = (addr >> 4) & (0x7f);
+	ttag = (addr >> 11);
+	offset = (addr & 0xf);
+	int i;
+	for (i = 0; i < LINE; i++)
+	{
+		if (L1[set][i].valid == true && L1[set][i].tag == ttag)
+		{
+			find = true;
+			break;
+		}
+	}
+	if (find == true)
+	{
+		uint32_t result[2];
+		memcpy(result, L1[set][i].data + (4 * offset), 4);
+		return result[0] & (~0u >> ((4 - len) << 3));
+	}
+	else
+	{
+		bool empty;
+		int j = 0;
+		for (j = 0; j < LINE; j++)
+		{
+			if(L1[set][j].valid == 0)
+			{
+				empty = true;
+				break;
+			}
+		}
+		if(empty)
+		{
+		   M2C(addr , set,j);
+		}
+		else /*cache full*/
+		{
+              int p =get_num();
+			  M2C(addr,set ,p);
+		}
+		uint32_t result[2];
+		memcpy(result, L1[set][i].data + (4 * offset), 4);
+		return result[0] & (~0u >> ((4 - len) << 3));
+	}
+}
+
+
+void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data)
+{
 	dram_write(addr, len, data);
 }
 
-uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
+uint32_t lnaddr_read(lnaddr_t addr, size_t len)
+{
 	return hwaddr_read(addr, len);
 }
 
-void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
+void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data)
+{
 	hwaddr_write(addr, len, data);
 }
 
-uint32_t swaddr_read(swaddr_t addr, size_t len) {
+uint32_t swaddr_read(swaddr_t addr, size_t len)
+{
 #ifdef DEBUG
-/*if len != 1 2 4 ,assert)*/
+	/*if len != 1 2 4 ,assert)*/
 	assert(len == 1 || len == 2 || len == 4);
 #endif
 	return lnaddr_read(addr, len);
 }
 
-void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
+void swaddr_write(swaddr_t addr, size_t len, uint32_t data)
+{
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
