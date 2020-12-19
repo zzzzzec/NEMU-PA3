4bb0bb7c09eecbb9db5cb4270a94b78380c2b014
tracer-ics2016 <tracer@njuics.org>
2020-12-19 16:14:27 +0800

    >  compile NEMU 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:14:27 up  2:24,  1 user,  load average: 0.30, 0.60, 0.73 34af64024bc2711137f019221ba4d72065c4dfe2

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -49,7 +49,7 @@ void M2C(hwaddr_t addr,uint32_t set ,int line){
 			{
 				tem[k] = dram_read(addr, 4);
 				addr += 4;
-				printf("%d ",dram_read(addr , 4));
+				printf("0x%08x ",dram_read(addr , 4));
 			}
 			memcpy(tem , L1[set][line].data , 64);
 		  	for ( k = 0; k < 16; k++)
