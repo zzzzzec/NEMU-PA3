90392c1bc585c4b97ca2357506b9c4fe7d05b2c6
tracer-ics2016 <tracer@njuics.org>
2020-12-20 16:31:14 +0800

    >  run 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:31:14 up 34 min,  1 user,  load average: 0.16, 0.32, 0.42 aa0839ee92623559c07a6f41d28379e3e57a896c

--- testcase/src/cache.c
+++ testcase/src/cache.c
@@ -4,10 +4,15 @@ int main(){
     int a[Cachesize/4];
     int  i;
     int k = 0;
-    for ( i = 0; i < (Cachesize/4); i++)
+    /*for ( i = 0; i < (Cachesize/4); i++)
     {
         a[i] =k ;
+    }*/
+    for ( i = 0; i <(Cachesize/4); i = i + 65)
+    {
+        a[i]=k;
     }
+    
     return 0;
 }
 
