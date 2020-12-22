5abceb5d16183a38adacbe23d9695e469ad07fde
tracer-ics2016 <tracer@njuics.org>
2020-12-22 10:21:27 +0800

    >  compile NEMU 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:21:27 up 19:33,  1 user,  load average: 0.57, 0.41, 0.28 7e13484872b560665ccd529741eeb6da71273353

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -3,10 +3,40 @@
 
 #include "common.h"
 
-enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
-enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
-
+enum
+{
+	R_EAX,
+	R_ECX,
+	R_EDX,
+	R_EBX,
+	R_ESP,
+	R_EBP,
+	R_ESI,
+	R_EDI
+};
+enum
+{
+	R_AX,
+	R_CX,
+	R_DX,
+	R_BX,
+	R_SP,
+	R_BP,
+	R_SI,
+	R_DI
+};
+enum
+{
+	R_AL,
+	R_CL,
+	R_DL,
+	R_BL,
+	R_AH,
+	R_CH,
+	R_DH,
+	R_BH
+};
+enum {R_CS,R_DS,R_ES,R_SS};
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
  * access cpu.gpr[3]._16, we will get the `bx' register; if we access
@@ -17,56 +47,62 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 struct SEG_REG
 {
 	uint16_t RPL : 2;
-	uint16_t TI     : 1;
+	uint16_t TI : 1;
 	uint16_t index : 13;
 };
 
-typedef struct {
-     union{
-        union {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-	} gpr[8];
+typedef struct
+{
+	union
+	{
+		union
+		{
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
-	/* Do NOT change the order of the GPRs' definitions. */
+		/* Do NOT change the order of the GPRs' definitions. */
 
-	struct{
-            uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-        };
-     };
+		struct
+		{
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+	};
 
-     swaddr_t eip;
+	swaddr_t eip;
 
-     union {
-		struct {
-			uint32_t CF		:1;
-			uint32_t pad0	:1;
-			uint32_t PF		:1;
-			uint32_t pad1	:1;
-			uint32_t AF		:1;
-			uint32_t pad2	:1;
-			uint32_t ZF		:1;
-			uint32_t SF		:1;
-			uint32_t TF		:1;
-			uint32_t IF		:1;
-			uint32_t DF		:1;
-			uint32_t OF		:1;
-			uint32_t IOPL	:2;
-			uint32_t NT		:1;
-			uint32_t pad3	:1;
+	union
+	{
+		struct
+		{
+			uint32_t CF : 1;
+			uint32_t pad0 : 1;
+			uint32_t PF : 1;
+			uint32_t pad1 : 1;
+			uint32_t AF : 1;
+			uint32_t pad2 : 1;
+			uint32_t ZF : 1;
+			uint32_t SF : 1;
+			uint32_t TF : 1;
+			uint32_t IF : 1;
+			uint32_t DF : 1;
+			uint32_t OF : 1;
+			uint32_t IOPL : 2;
+			uint32_t NT : 1;
+			uint32_t pad3 : 1;
 			uint16_t pad4;
 		};
 		uint32_t val;
 	} eflags;
-    
-		struct 
-		{
-		uint32_t base ;
-		uint16_t limit;	
-		}gdtr;
 
-	union 
+	struct
+	{
+		uint32_t base;
+		uint16_t limit;
+	} gdtr;
+
+	union
 	{
 		struct
 		{
@@ -74,15 +110,30 @@ typedef struct {
 			struct SEG_REG DS;
 			struct SEG_REG ES;
 			struct SEG_REG SS;
-		} ;
+		};
 		struct SEG_REG sr[4];
 	};
-	
+
+	union
+	{
+		struct
+		{
+			uint32_t PE : 1;
+			uint32_t MP : 1;
+			uint32_t EM : 1;
+			uint32_t TS : 1;
+			uint32_t ET : 1;
+			uint32_t       : 26;
+			uint32_t PG : 1;
+		};
+		uint32_t CR0;
+	};
 } CPU_state;
 
 extern CPU_state cpu;
 
-static inline int check_reg_index(int index) {
+static inline int check_reg_index(int index)
+{
 	assert(index >= 0 && index < 8);
 	return index;
 }
@@ -91,8 +142,8 @@ static inline int check_reg_index(int index) {
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
 
-extern const char* regsl[];
-extern const char* regsw[];
-extern const char* regsb[];
+extern const char *regsl[];
+extern const char *regsw[];
+extern const char *regsb[];
 
 #endif
