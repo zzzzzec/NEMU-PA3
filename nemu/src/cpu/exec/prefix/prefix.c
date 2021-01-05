c06f5c1e43608cced934ff82259c3110f562ce36
tracer-ics2016 <tracer@njuics.org>
2021-01-05 09:55:52 +0800

    >  compile NEMU 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:55:52 up 33 min,  1 user,  load average: 0.62, 0.41, 0.38 87ffaeb04309f4ad8851e3919796accb319a401

--- nemu/src/cpu/exec/prefix/prefix.c
+++ nemu/src/cpu/exec/prefix/prefix.c
@@ -3,9 +3,9 @@
 make_helper(exec);
 
 make_helper(operand_size) {
-	printf("2 \n");
 	ops_decoded.is_operand_size_16 = true;
 	int instr_len = exec(eip + 1);
 	ops_decoded.is_operand_size_16 = false;
+	assert(false);
 	return instr_len + 1;
 }
