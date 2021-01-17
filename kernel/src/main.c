3d6dfa8174cf3d7d3e5969502417caa152c72ba8
tracer-ics2016 <tracer@njuics.org>
2021-01-17 16:02:50 +0800

    >  compile kernel 10602121 zhangenci Linux zhangenci-VirtualBox 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:02:50 up  2:26,  1 user,  load average: 0.44, 0.45, 0.41 838eeb49441751bacd89a72c8fdfffa39e828097

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -20,6 +20,7 @@ void init_cond();
  * The assembly code in start.S will finally jump here.
  */
 void init() {
+	panic("init \n");
 #ifdef IA32_PAGE
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
